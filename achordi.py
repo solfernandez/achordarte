9357352d615e374b657a4ee8f08f27c17f96d30a
Sol Fernandez <solfernandezarancibia@gmail.com>
2023-06-30 14:48:08 -0300

    add support to pass input filename from command line

--- achordi.py
+++ achordi.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 from achordarte.main import main
+import argparse
 
+parser = argparse.ArgumentParser(
+                    prog='Achordarte',
+                    description='Find chords play on a MIDI instrument',
+                    epilog='.')
+parser.add_argument('-i', '--input-file', default='/dev/snd/midiC1D0')
 
-input_file = "/dev/snd/midiC1D0"
+#input_file = "/dev/snd/midiC1D0"
+input_file = parser.parse_args().input_file
 main(input_file)
